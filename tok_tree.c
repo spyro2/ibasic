ed1104f839a9536ec6ef9edf59041fe5bb04d7a5
Ian Molton <ian@mnementh.co.uk>
2016-04-24 14:04:03 +0100

    Add tests for bad characters.

--- tok_tree.c
+++ tok_tree.c
@@ -306,6 +306,9 @@ int main (void) {
 	tokenise(tok_tree, "L ");
 	tokenise(tok_tree, "L  ");
 	tokenise(tok_tree, "LABLE");
+	tokenise(tok_tree, "5 PR\nOC");
+	tokenise(tok_tree, "10 PR\n\nOC");
+	tokenise(tok_tree, "15 PR\n\n\nOC");
 	tokenise(tok_tree, "FLOOBPRINT   PRINTIF  GOTO PROC ENDPFOG   END ENDPROC   ENDPROD");
 	tokenise(tok_tree, "10 PRINT \"Hello!\"\r");
 	tokenise(tok_tree, "20 INPUT A$\r");
