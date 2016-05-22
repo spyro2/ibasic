3647024e8df13ab261738dc310e3aa1f254c52fa
Ian Molton <ian@mnementh.co.uk>
2016-05-22 20:26:10 +0100

    Tokeniser: Clean up macros for valid characters

--- tokeniser.c
+++ tokeniser.c
@@ -347,8 +347,10 @@ static int tok_add(struct tok_tree_entry **ptte, struct token *t, char *c) {
 }
 
 #define IS_WS(c) ((c)==' ' || (c)=='\t')
+#define IS_DIGIT(c) ((c) >= '0' && (c) <= '9')
+#define IS_ALPHA(c) (((c) >= 'a' && (c) <= 'z') || ((c) >= 'A' && (c) <= 'Z'))
+#define IS_LABEL(c) (IS_DIGIT(c) || IS_ALPHA(c) || ((c) == '_') || ((c) == '.'))
 
-#define IS_LABEL(c) (((c) >= '0' && (c) <= '9') || ((c) >= 'a' && (c) <= 'z') || ((c) >= 'A' && (c) <= 'Z') || ((c) == '_'))
 
 static struct line_entry *extract_label(char **ps) {
 	char *s = *ps;
