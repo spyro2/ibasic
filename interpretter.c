bf234a9d262db21cc1e0d51fbb08d62bffabe387
Ian Molton <ian@mnementh.co.uk>
2016-06-05 11:20:53 +0100

    Interpretter: Implement a very simple call/return stack.

--- interpretter.c
+++ interpretter.c
@@ -5,6 +5,18 @@
 void interpret(struct ast_entry *p) {
 static struct value *var[10];
 
+static struct value *call_stack[10];
+static struct value **call_stack_p = &call_stack[0];
+
+void val_push(struct value *v) {
+	*call_stack_p = v;
+	call_stack_p++;
+}
+
+struct value *val_pop(void) {
+	call_stack_p--;
+	return *call_stack_p;
+}
 
 struct value *lookup_var(char *name) {
 	int i = *name-'A';
--- interpretter.h
+++ interpretter.h
@@ -1,3 +1,4 @@
 void interpret(struct ast_entry *p);
 struct value *lookup_var(char *name);
+void val_push(struct value *v);
 
