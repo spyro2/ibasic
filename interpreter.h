35d3f5594f7f05465f652d13bb6d1c43f12f1ec7
Ian Molton <ian@mnementh.co.uk>
2016-06-12 02:51:19 +0100

    Interpreter: clean up header.

--- interpreter.h
+++ interpreter.h
@@ -2,9 +2,10 @@ void interpret(struct ast_entry *p);
 int interpret_block(struct ast_entry *e, struct value *r);
 
 struct value *lookup_var(char *name);
+
 struct value *val_alloc(char *name);
+struct value *val_pop(void);
+
 struct value *val_alloc_frame(void);
 void val_set_frame(struct value *v);
 void unwind_frame(void);
-struct value *val_pop(void);
-
